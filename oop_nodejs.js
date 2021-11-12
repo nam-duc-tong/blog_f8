13dcc501bcda5693fc1217a6b1caca1114216274
Tống Đức Nam <637849@sv.vnua.edu.vn>
2021-11-12 15:33:59 +0700

    add new fle log

---
+++ oop_nodejs.js
@@ -0,0 +1,7 @@
+var mylog = require('./log.js');
+mylog.info('Thong bao tu trang web');
+mylog.warning('Canh bao!!!!');
+mylog.error('Loi roi!!!');
+// console.log(mylog.info);
+// console.log(mylog.warning);
+// console.log(mylog.error);
