f7df4fad97915925c9add9c95d36035b3cf0c59e
Tống Đức Nam <637849@sv.vnua.edu.vn>
2021-11-12 10:05:42 +0700

    update_server_js

--- webserver/server.js
+++ webserver/server.js
@@ -1,27 +1,55 @@
+// var http = require('http');
+// var server = http.createServer(function(req,res){
+//     if(req.url=="/"){
+//         //check the url of the current resquest
+        
+//         //set response header
+//         res.writeHead(200,{'Content-Type':'text/html'});
+//         // set response Content
+//         res.write('<html><body><p>This is home Page.</p></body></html>');
+//         res.end();
+//     }
+//     else if(req.url=="/student"){
+//         res.writeHead(200,{'Content-Type':'text/html'});
+//         res.write('<html><body><p>This is Student Page.</p></body></html>');
+//         res.end();
+//     }
+//     else if(req.url=="/admin"){
+//         res.writeHead(200,{'Content-Type':'text/html'});
+//         res.write('<html><body><p>This is admin Page.</p></body></html>');
+//         res.end();
+//     }
+//     else{
+//         res.end("Invalid Request");
+//     }
+// });
+// server.listen(5000);
+// console.log('Node.js web server at port 5000 is running...');
+
+
 var http = require('http');
 var server = http.createServer(function(req,res){
     if(req.url=="/"){
-        //check the url of the current resquest
-        
-        //set response header
         res.writeHead(200,{'Content-Type':'text/html'});
-        // set response Content
         res.write('<html><body><p>This is home Page.</p></body></html>');
         res.end();
     }
     else if(req.url=="/student"){
         res.writeHead(200,{'Content-Type':'text/html'});
-        res.write('<html><body><p>This is Student Page.</p></body></html>');
+        res.write('<html><body><p>This is student Page.</p></body></html>');
         res.end();
     }
-    else if(req.url=="/admin"){
+    else if(req.url =="/admin"){
         res.writeHead(200,{'Content-Type':'text/html'});
         res.write('<html><body><p>This is admin Page.</p></body></html>');
         res.end();
     }
-    else{
-        res.end("Invalid Request");
-    }
+    else
+    res.end('Invalid Request!');
 });
 server.listen(5000);
 console.log('Node.js web server at port 5000 is running...');
+
+
+
+
